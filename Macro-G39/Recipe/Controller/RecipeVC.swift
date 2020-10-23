bd18eaa2b883a13ddbcee309367c1d5d81f8a5bb
Muhammad Rivan Rayhan <rayhanrivan@gmail.com>
2020-10-23 10:49:22 +0700

    Add RecipeCollectionView

--- Macro-G39/Recipe/Controller/RecipeVC.swift
+++ Macro-G39/Recipe/Controller/RecipeVC.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class RecipeVC: UIViewController {
 
+    @IBOutlet weak var recipeCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        recipeCollectionView.register(UINib.init(nibName: "RecipeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "recipeCell")
+        recipeCollectionView.delegate = self
+        recipeCollectionView.dataSource = self
     }
 
 
+}
+extension RecipeVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as! RecipeCollectionViewCell
+        return cell
+    }
+    
+    
 }
---
+++ Macro-G39/Recipe/View/RecipeCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CollectionViewCell.swift
+//  Macro-G39
+//
+//  Created by Muhammad Rivan Rayhan on 23/10/20.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var RecipeView: UIView!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var bananaRipenessLabel: UILabel!
+    @IBOutlet weak var bananaTypeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.RecipeView.layer.cornerRadius = 20
+        self.RecipeView.layer.borderColor = UIColor.gray.cgColor
+        self.RecipeView.layer.borderWidth = 1
+        // Initialization code
+    }
+
+}
