83d7b18ddc5e012940ed74b7896c1b3401c38016
Muhammad Rivan Rayhan <rayhanrivan@gmail.com>
2020-10-26 08:10:27 +0700

    Add Home CollectionView for recipe

--- Macro-G39/Home/Controller/HomeVC.swift
+++ Macro-G39/Home/Controller/HomeVC.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var homeCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        homeCollectionView.register(UINib.init(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "homeCell")
+        
+        homeCollectionView.dataSource = self
+        homeCollectionView.delegate = self
     }
     @IBAction func goToRecipePage(_ sender: Any) {
         performSegue(withIdentifier: "segueToRecipe", sender: nil)
+    }
+    @IBAction func goToCameraPage(_ sender: UIButton) {
+        performSegue(withIdentifier: "segueToCamera", sender: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,4 +33,18 @@ class HomeVC: UIViewController {
         }
     }
 
+}
+
+extension HomeVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = homeCollectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath) as! HomeCollectionViewCell
+        cell.recipeNameLabel.text = "JALAN"
+        return cell
+    }
+    
+    
 }
