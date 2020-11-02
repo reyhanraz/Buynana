7399709862451ea755e94c10d7c2bed930823c85
Muhammad Rivan Rayhan <rayhanrivan@gmail.com>
2020-11-02 08:38:15 +0700

    add blackoverlay for collectionview

--- Macro-G39/Recipe/View/RecipeCollectionViewCell.swift
+++ Macro-G39/Recipe/View/RecipeCollectionViewCell.swift
@@ -15,7 +15,7 @@ class RecipeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bananaRipenessLabel: UILabel!
     @IBOutlet weak var bananaTypeLabel: UILabel!
     @IBOutlet weak var blackOverlay: UIView!
-
+    let gl = CAGradientLayer()
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,6 +24,14 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         self.RecipeView.layer.borderColor = UIColor.gray.cgColor
         self.RecipeView.layer.borderWidth = 1
         // Initialization code
+        
+        
+        
+        gl.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gl.locations = [0.0,1.0]
+        gl.frame = blackOverlay.frame
+        blackOverlay.layer.addSublayer(gl)
+        blackOverlay.layer.cornerRadius = 20
+        blackOverlay.clipsToBounds = true
     }
-
 }
